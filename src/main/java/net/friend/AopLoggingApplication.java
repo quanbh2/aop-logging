4f72b9b40c41af3a309adf5932037d0147eefc9c
quanbh2 <quanbh2@topica.edu.vn>
2019-10-08 16:22:17 +0700

    clean trash

--- src/main/java/net/friend/AopLoggingApplication.java
+++ src/main/java/net/friend/AopLoggingApplication.java
@@ -4,7 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.crypto.bcrypt.BCrypt;
 
 @Slf4j
 @SpringBootApplication
@@ -16,14 +15,5 @@ public class AopLoggingApplication implements CommandLineRunner {
 
   @Override
   public void run(String... args) throws Exception {
-
-    String pass1  = BCrypt.hashpw("12345678", BCrypt.gensalt(12));
-    log.info("pass1 : {}", pass1);
-
-    log.info(BCrypt.hashpw("12345678", BCrypt.gensalt(12)).replace("$2a$", "$2y$"));
-
-    log.info(BCrypt.hashpw("12345678", BCrypt.gensalt(12)).replace("$2a$", "$2y$"));
-
-
   }
 }
